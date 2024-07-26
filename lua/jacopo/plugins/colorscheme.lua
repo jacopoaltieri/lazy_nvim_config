b626efd0a8a589d22e9acc5df237a469e359a48f
jacopoaltieri <jacopo.altieri@gmail.com>
2024-07-26 09:58:50 +0200

    Changed colorscheme to Catppuccin frappe

--- lua/jacopo/plugins/colorscheme.lua
+++ lua/jacopo/plugins/colorscheme.lua
@@ -1,40 +1,44 @@
 return {
 	{
-		"folke/tokyonight.nvim",
+		"catppuccin/nvim",
+		name = "catppuccin",
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			local bg = "#011628"
-			local bg_dark = "#011423"
-			local bg_highlight = "#143652"
-			local bg_search = "#0A64AC"
-			local bg_visual = "#275378"
-			local fg = "#CBE0F0"
-			local fg_dark = "#B4D0E9"
-			local fg_gutter = "#627E97"
-			local border = "#547998"
+			local custom_colors = {
+				bg = "#303446",
+				bg_dark = "#292C3C",
+				bg_highlight = "#44475A",
+				bg_popup = "#292C3C",
+				bg_search = "#BD93F9",
+				bg_sidebar = "#292C3C",
+				bg_statusline = "#292C3C",
+				bg_visual = "#6272A4",
+				border = "#6272A4",
+				fg = "#F8F8F2",
+				fg_dark = "#E6E6E6",
+				fg_float = "#F8F8F2",
+				fg_gutter = "#4B5263",
+				fg_sidebar = "#E6E6E6",
+			}
 
-			require("tokyonight").setup({
-				style = "night",
-				on_colors = function(colors)
-					colors.bg = bg
-					colors.bg_dark = bg_dark
-					colors.bg_float = bg_dark
-					colors.bg_highlight = bg_highlight
-					colors.bg_popup = bg_dark
-					colors.bg_search = bg_search
-					colors.bg_sidebar = bg_dark
-					colors.bg_statusline = bg_dark
-					colors.bg_visual = bg_visual
-					colors.border = border
-					colors.fg = fg
-					colors.fg_dark = fg_dark
-					colors.fg_float = fg
-					colors.fg_gutter = fg_gutter
-					colors.fg_sidebar = fg_dark
+			require("catppuccin").setup({
+				flavour = "frappe",
+				custom_highlights = function(colors)
+					return {
+						Normal = { bg = custom_colors.bg, fg = custom_colors.fg },
+						NormalFloat = { bg = custom_colors.bg_popup, fg = custom_colors.fg_float },
+						Visual = { bg = custom_colors.bg_visual },
+						StatusLine = { bg = custom_colors.bg_statusline, fg = custom_colors.fg },
+						StatusLineNC = { bg = custom_colors.bg_statusline, fg = custom_colors.fg_gutter },
+						VertSplit = { fg = custom_colors.border },
+						LineNr = { fg = custom_colors.fg_gutter },
+						CursorLineNr = { fg = custom_colors.fg },
+						-- Add more highlights as needed
+					}
 				end,
 			})
 			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
+			vim.cmd([[colorscheme catppuccin-frappe]])
 		end,
 	},
 }
